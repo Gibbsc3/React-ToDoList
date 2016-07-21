7e79473de8244b8ea165bb304e98f6aba5927729
Tony <gibbsc@indiana.edu>
2016-07-21 14:42:07 -0400

    clean

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import TodoList from './TodoList'
- // import Lifecycle from './Lifecycle'
-//name is a props. call it with this.props.value (ie tony)
- // ReactDOM.render(<Lifecycle />, document.getElementById('app'))
- // ReactDOM.render(<TodoList />, document.getElementById('app'))
 
  ReactDOM.render(<TodoList />, document.getElementById('app'))
